cdcc3622ed99c466037fba0b9b465b840cd4daef
AlekseiS314 <debosh1r5432100@mail.ru>
2025-05-29 18:26:15 +0300

    maybe

--- router.py
+++ router.py
@@ -6,14 +6,19 @@ class RequestUpdate(BaseModel):
     new_string: str
 
 @router.get("/get_map")
-def get_game_map():
+def get_map():
     file = open("game_map.txt","r")
     string = file.read()
     return {"status": string}
 
 @router.patch("/update_map")
-def update_game_map(body: RequestUpdate):
+def update_map(body: RequestUpdate):
     file = open("game_map.txt","w")
     file.write(body.new_string)
     return {"status": body.new_string}
 
+@router.post("/set_name/{name}")
+def set_name(name : str):
+    file = open("Name_players.txt","a")
+    file.write("\n"+name)
+    return {"status": "\n"+name}
